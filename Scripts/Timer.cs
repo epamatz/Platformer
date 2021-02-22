e03bb6fec602fdf1297550bbdfe0ab95be67fdad
epamatz <40709011+epamatz@users.noreply.github.com>
2021-02-21 23:33:24 -0800

    Add files via upload

---
+++ Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+     public float timer = 0.0f;
+     public int seconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     void Update()
+     {
+         timer += Time.deltaTime;
+         seconds = (int)(timer % 60);
+         Debug.Log(seconds);
+     }
+}
